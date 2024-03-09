533cd5edf4b5626b8cda96eb99ad0256b00ae82a
NexusNV <154760436+SpectreVC@users.noreply.github.com>
2024-03-09 18:16:35 +0200

    Update MainSrc.lua

--- MainSrc.lua
+++ MainSrc.lua
@@ -388,8 +388,8 @@ end
 
 local function CreateScreenToggle()
 local Button = Instance.new("TextButton")
-Button.Size = UDim2.new(0,40,0,50)
-Button.Position = UDim2.new(0,100,0,10)
+Button.Size = UDim2.new(0,60,0,50)
+Button.Position = UDim2.new(0,60,0,10)
 Button.Text = ""
 Button.BackgroundColor3 = Color3.new(0.1,0.1,0.1)
 Button.BorderSizePixel = 0
